e18b3f0e8281b1c76ab28a81b649549ed1df7d13
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-07 15:25:02 +0100

    Exercise 1 and 2 Update
    The above exercises have been completed successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(myStr, myVal) {
+    let myOutput='';
+    if(!isNaN(myVal) && myVal < 0){
 
+        myOutput = 'ERROR';
+
+    }else{
+
+        for (let i=0; i < myVal; i++){
+            myOutput += myStr;
+        }
+    }
+    return myOutput.replace(/\s/g,'')
 };
 
 // Do not edit below this line
