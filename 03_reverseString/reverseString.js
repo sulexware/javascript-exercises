451dfddf50b3ef8135f4fc45dd0422976175bf1f
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-07 15:46:37 +0100

    Exercise 3 Update
    The above exercises have been completed successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(myStr) {
+    let myOutput ='';
+
+    if(myStr !== ''){
+
+        let myArray = myStr.split("");
+
+        for(let i = myArray.length - 1; i >= 0; i--){
+
+            myOutput += myArray[i];
+        }
+
+    }
+
+    return myOutput
 
 };
 
