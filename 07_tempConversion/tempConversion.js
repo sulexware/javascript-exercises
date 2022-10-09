3e01dcd7287a351fd9072530dcb01a8fb7a335f2
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-09 19:06:19 +0100

    Exercise 7 Update
    The exercise above have been completed successfully

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(arg1) {
+  return round(((arg1-32)*(5/9)), 1)
 };
 
-const ctof = function() {
-
+const ctof = function(arg1) {
+  return round((arg1 * 9/5 + 32) , 1)
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
