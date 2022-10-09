2aac6478e57453cf552d962ae756f94a2a93c717
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-09 17:27:15 +0100

    Exercise 4 Update
    The above exercise has been completed succussfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arg1, ...arg2) {
+    let i = 0;
+        for (const arg of arg2){
+            for(const element of arg1){
+                if( arg === element){
+                    arg1.splice(i, 1);
+                }
 
+                i++;
+            }
+            
+           i=0;
+        }
+
+        return arg1
 };
 
 // Do not edit below this line
