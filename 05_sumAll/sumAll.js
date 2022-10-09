37982e49b8f5f60ddd7944ad60f8663c805f6320
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-09 17:55:12 +0100

    Exercise 5 Update
    The above exercise have been completed successufully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    if(arg1 < 0 || arg2 < 0 || isNaN(arg1) || isNaN(arg2) || typeof arg1 === 'string' || typeof arg2 === 'string'){
 
+        sum ='ERROR';
+    }else{
+        if(arg2 > arg1){
+            for(let i = arg1; i <= arg2; i++){
+                sum +=i;
+            }
+        }else{
+            for(let i = arg2; i <= arg1; i++){
+                sum +=i;
+            }
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
