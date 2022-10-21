323cf7bffd6718bc7b1e00751395513ab8b3e788
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-21 10:13:06 +0100

    Exercise 8 - Calculator
    The objectives of this exercise has been met

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  	return args.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.length ? args.reduce((totalz, itemz) => totalz * itemz) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0) return 1
+  let f = 1;
+  for(let i =n; i > 0; i--){
+    f *= i;
+  }
+
+  return f;
 };
 
 // Do not edit below this line
