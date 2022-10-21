d39fef7701307dcbd889cb5e2a1f4b2cca8fa7da
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-21 21:24:11 +0100

    Exercise 9 - Palindromes
    The objectives of this exercise has been met

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let re =/[^a-z]/g;
+  let lowReStr = str.toLowerCase().replace(re,'');
+  console.log(lowReStr);
+  let reStr = lowReStr.split('').reverse().join('');
+  return reStr === lowReStr
 };
 
 // Do not edit below this line
