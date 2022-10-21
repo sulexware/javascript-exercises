5f96681e3798f60990240e851a71b0ca78ea0742
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-21 22:20:22 +0100

    Exercise 10 Fibonacci
    The objectives of the exercise have been met

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  /*
+    if(Math.sign(num) === -1) return 'OOPS'
+  num = parseInt(num);
+  if(num === 1) return 1
+  if(num === 2) return 1
+  let num1 = 0;
+  let num2 = 1;
+  let sum;
+  
+  for (let i = 2; i <= num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return sum
+  */
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < num; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
