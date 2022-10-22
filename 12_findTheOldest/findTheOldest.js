bbc10d659b467c28b36499caa86fd1ba57059dd1
Suleiman Bashir Sunusi <sulexware@gmail.com>
2022-10-22 11:38:56 +0100

    Exercise 12 finTheOldest
    The objectives have been met

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(objs) {
 
+    return objs.reduce((itema, itemb) => {
+        const first = getAge(itema.yearOfBirth, itema.yearOfDeath);
+        const second = getAge(itemb.yearOfBirth, itemb.yearOfDeath);
+        return first < second ? itemb : itema
+    });
+ 
 };
+
+const getAge = function(myDOB, myDOD){
+    if(!myDOD)
+      myDOD = new Date().getFullYear();
+    return myDOD - myDOB
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
